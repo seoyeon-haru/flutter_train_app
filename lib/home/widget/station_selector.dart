e65d7015bbee0d89dfbc0ef61c3d079a1e6b478d
seoyeon-haru <sy5792@naver.com>
2025-07-13 21:18:24 +0900

    Implement departure and arrival in station_selector.dart

--- lib/home/widget/station_selector.dart
+++ lib/home/widget/station_selector.dart
@@ -9,6 +9,17 @@ class StationSelector extends StatelessWidget {
         color: Colors.white,
         borderRadius: BorderRadius.circular(20), // 모서리 둥글기
       ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text('출발역',  // 텍스트 입력
+          style: TextStyle(fontSize: 16, // 텍스트 스타일 변경
+          color: Colors.grey,
+          fontWeight: FontWeight.bold),),
+          Text('도착역', style: TextStyle(fontSize: 16, // 텍스트 입력
+          color: Colors.grey, fontWeight: FontWeight.bold),) // 텍스트 스타일 변경
+        ],
+      ),
     );
   }
 }
