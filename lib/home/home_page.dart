cf9707344586df16473a188ed232cc349e0abc3f
seoyeon-haru <sy5792@naver.com>
2025-07-13 22:34:50 +0900

    Apply rounded corners

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -10,18 +10,23 @@ class HomePage extends StatelessWidget {
       ),
       backgroundColor: Colors.grey[200], // 배경 색상 설정
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20), // 전반적으로 패딩 설정
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             StationSelector(),
-            SizedBox(height: 20),
+            SizedBox(height: 20), // 간격
             SizedBox(
-              width: double.infinity,
+              width: double.infinity, 
               height: 50,
               child: ElevatedButton(
                   onPressed: () {},
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple, // 버튼 배경색 설정
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20), // 버튼 모서리 둥글기
+                    ),
+                  ),
                   child: Text(
                     '좌석 선택',
                     style: TextStyle(
