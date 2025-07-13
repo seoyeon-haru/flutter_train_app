5801df18cee4e6670f038d228dca0f10b58ec8fa
seoyeon-haru <sy5792@naver.com>
2025-07-13 20:27:42 +0900

    Add box in station_selector.dart

--- lib/home/widget/station_selector.dart
+++ lib/home/widget/station_selector.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class StationSelector extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('StationSelector');
+    return Container(
+      height: 200,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20), // 모서리 둥글기
+      ),
+    );
   }
 }
