694a8fad537f75c45ac1173fa7d955b225aa1cae
seoyeon-haru <sy5792@naver.com>
2025-07-14 14:11:50 +0900

    Add AppBar

--- lib/home/widget/station_selector.dart
+++ lib/home/widget/station_selector.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/second/station_list_page.dart';
+import 'package:flutter_train_app/third/arrive_list_page.dart';
 
 class StationSelector extends StatelessWidget {
   @override
@@ -12,8 +14,9 @@ class StationSelector extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Column( // 출발역 Text Wrap with Column 해서 선택 글자 구현
-          mainAxisAlignment: MainAxisAlignment.center,
+          Column(
+            // 출발역 Text Wrap with Column 해서 선택 글자 구현
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 '출발역', // 텍스트 입력
@@ -22,13 +25,24 @@ class StationSelector extends StatelessWidget {
                     color: Colors.grey,
                     fontWeight: FontWeight.bold),
               ),
-              Text(
-                '선택',
-                style: TextStyle(fontSize: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return StationListPage();
+                    },
+                  ));
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.white, elevation: 0),
+                child: Text('선택', style: TextStyle(fontSize: 40, color: Colors.black),),
               )
             ],
           ),
-          Container(width: 2, height: 50, color: Colors.grey[400],), // 가운데 선 구현
+          Container(
+            width: 2,
+            height: 50,
+            color: Colors.grey[400],
+          ), // 가운데 선 구현
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -39,7 +53,13 @@ class StationSelector extends StatelessWidget {
                     color: Colors.grey,
                     fontWeight: FontWeight.bold),
               ),
-              Text('선택', style: TextStyle(fontSize: 40),)
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ArriveListPage();
+                }));
+              }, 
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.white, elevation: 0),
+              child: Text('선택', style: TextStyle(fontSize: 40, color: Colors.black),),)
             ],
           ) // 텍스트 스타일 변경
         ],
--- lib/second/station_list_page.dart
+++ lib/second/station_list_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/second/widget/station_name_list.dart';
 
 class StationListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('출발역'),
+      ),
+      body: Column(
+        children: [
+          StationNameList(),
+        ],
+      ),
+    );
   }
 }
